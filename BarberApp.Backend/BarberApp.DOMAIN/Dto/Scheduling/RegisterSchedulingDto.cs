b0b68d71e520c793229fd26c8d29c6d6aacf028d
gabriel.britog <gabriel.britog@outlook.com>
2023-04-21 17:14:48 -0300

    (Implementação)Backend - Serviço de turmas e dados em clients

--- BarberApp.Backend/BarberApp.DOMAIN/Dto/Scheduling/RegisterSchedulingDto.cs
+++ BarberApp.Backend/BarberApp.DOMAIN/Dto/Scheduling/RegisterSchedulingDto.cs
@@ -15,8 +15,8 @@ namespace BarberApp.Domain.Dto.Scheduling
     {
         [BsonElement("client")]
         public RegisterClientDto Client { get; set; } = null!;
-        [BsonElement("listClient")]
-        public List<RegisterClientDto> ListClient { get; set; } = null!;
+        [BsonElement("class")]
+        public Class Class { get; set; } = null!;
         [BsonElement("serviceType")]
         public List<RegisterServiceTypeDto> ServiceType { get; set; }
         [BsonElement("total")]
---
+++ BarberApp.Backend/BarberApp.DOMAIN/Enums/CivilStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarberApp.Domain.Enums
+{
+    public enum CivilStatus
+    {
+        Single = 0,
+        Married = 1,
+        Divorced = 2,
+        Widowed = 3,
+        Engaged = 4,
+        CommonLawMarrige = 5
+    }
+}
