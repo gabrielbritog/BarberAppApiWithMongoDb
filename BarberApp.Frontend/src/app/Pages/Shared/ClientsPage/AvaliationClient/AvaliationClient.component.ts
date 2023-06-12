80d88cff8477ea8c2a6af625fc0061468c0110d1
Moises Marchini <moisesmarchinipereira@gmail.com>
2023-06-12 20:42:22 -0300

    Finalizado Ficha de avaliação

--- BarberApp.Frontend/src/app/Pages/Shared/ClientsPage/AvaliationClient/AvaliationClient.component.ts
+++ BarberApp.Frontend/src/app/Pages/Shared/ClientsPage/AvaliationClient/AvaliationClient.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import * as moment from 'moment';
+import { Subscription } from 'rxjs';
 import { IFormInput } from 'src/app/Components/FormInput/IFormInput';
+import { GlobalVariables } from 'src/app/Helpers/GlobalVariables';
+import { environment } from 'src/app/Helpers/environment';
+import { BarberModel } from 'src/app/Models/BarberModel';
+import { ClientModel, ClientModelHelper } from 'src/app/Models/ClientModel';
+import { ClientService } from 'src/app/Services/api/Client.service';
+import { RouteHistoryService } from '../../../../Services/misc/route-history.service';
 
 const dateTimeFormat = 'YYYY-MM-DD'
 
@@ -11,7 +19,10 @@ const dateTimeFormat = 'YYYY-MM-DD'
 })
 export class AvaliationClientComponent implements OnInit {
 
-  constructor() { }
+  id: string = '';
+  subscription?: Subscription;
+
+  clientModel: ClientModel = ClientModelHelper.create();
 
   modalInputs: IFormInput[] =
     [
@@ -140,19 +151,223 @@ export class AvaliationClientComponent implements OnInit {
       },
     ]
 
+  get isUserLevelToEdit() {
+    return GlobalVariables.userLevel <= environment.userLevel.readAndEdit;
+  }
+
+  constructor(
+    private clientsService: ClientService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private routeHistory: RouteHistoryService,
+  ) { }
+
   ngOnInit() {
-    this.modalInputs.forEach(p => {
-      if (p.options)
-        p.options.required = false;
-      else
-        p.options = {
-        required: false
-      };
-    })
+
+    if (!this.activatedRoute.snapshot.params['id']){
+      this.router.navigateByUrl('/Clients');
+      return;
+    }
+
+    this.subscription =
+      this.activatedRoute.params.subscribe(
+        (params: any) => {
+          if (!params['id']){
+            this.router.navigateByUrl('/Clients');
+            return;
+          }
+
+          this.id = params['id'];
+
+          const existedClientModel = GlobalVariables.clients.find(client => client.clientId === this.id);
+
+          if (!existedClientModel) {
+            this.router.navigateByUrl('/Clients');
+            return;
+          }
+
+          this.clientModel = ClientModelHelper.clone(existedClientModel);
+          this.updateModalInputs();
+        }
+      )
+  }
+
+  updateModalInputs() {
+    const evaluationSheet = this.clientModel.evaluationSheet;
+
+    const EmployeesAsFormOptions = () => {
+      return GlobalVariables.employees.map((employee: BarberModel, index: number) => {
+        return {
+          id: 'employee_' + index,
+          label: `${employee.firstName} ${employee.lastName}`,
+          value: employee.barberId,
+          isSelected: evaluationSheet?.appraiser === employee.barberId
+        }
+      });
+    }
+
+    function formatDatetime(date?: string) {
+      if (!date)
+        return moment().format(dateTimeFormat);
+
+      return moment(date).format(dateTimeFormat);
+    }
+    this.modalInputs =
+    [
+      {
+        id: 'entryTime',
+        label: 'Horário de entrada',
+        type: 'date',
+        value: formatDatetime(evaluationSheet?.entryTime),
+      },
+      {
+        id: 'departureTime',
+        label: 'Horário de saída',
+        type: 'date',
+        value: formatDatetime(evaluationSheet?.departureTime),
+      },
+      {
+        id: 'dateOfAssessment',
+        label: 'Data da avaliação',
+        type: 'date',
+        value: formatDatetime(evaluationSheet?.dateOfAssessment),
+      },
+      {
+        id: 'appraiser',
+        label: 'Avaliador',
+        type: 'select',
+        value: evaluationSheet?.appraiser?? '',
+        formOptions: EmployeesAsFormOptions()
+      },
+      {
+        id: 'physicalActivity',
+        label: 'Atividades físicas praticadas',
+        type: 'text',
+        value: evaluationSheet?.physicalActivity ?? '',
+        options: {
+          required: false
+        }
+      },
+      {
+        id: 'bodyProportions',
+        label: 'Proporções Corporais',
+        type: 'formgroup',
+        value: '',
+        formGroup: [
+          {
+            id: 'weight',
+            label: 'Peso',
+            type: 'text',
+            value: evaluationSheet?.bodyProportions.weight?? '',
+            options: {
+              mask: 'size',
+            }
+          },
+          {
+            id: 'height',
+            label: 'Altura',
+            type: 'text',
+            value: evaluationSheet?.bodyProportions.height?? '',
+            options: {
+              mask: 'size'
+            }
+          },
+          {
+            id: 'bloodPressure',
+            label: 'Pressão Sanguínea',
+            type: 'text',
+            value: evaluationSheet?.bodyProportions.bloodPressure?? '',
+          },
+          {
+            id: 'imc',
+            label: 'IMC',
+            type: 'text',
+            value: evaluationSheet?.bodyProportions.imc?? '',
+            options: {
+              mask: 'size'
+            }
+          },
+          {
+            id: 'classification',
+            label: 'Classificação',
+            type: 'text',
+            value: evaluationSheet?.bodyProportions.classification?? '',
+          },
+          {
+            id: 'waist',
+            label: 'Cintura',
+            type: 'text',
+            value: evaluationSheet?.bodyProportions.waist?? '',
+            options: {
+              mask: 'size'
+            }
+          },
+          {
+            id: 'hip',
+            label: 'Quadril',
+            type: 'text',
+            value: evaluationSheet?.bodyProportions.hip?? '',
+            options: {
+              mask: 'size'
+            }
+          },
+          {
+            id: 'abdomen',
+            label: 'Abdômen',
+            type: 'text',
+            value: evaluationSheet?.bodyProportions.abdomen?? '',
+            options: {
+              mask: 'size'
+            }
+          },
+        ]
+      },
+      {
+        id: 'details',
+        label: 'Detalhes',
+        type: 'textarea',
+        value: evaluationSheet?.details?? '',
+      },
+      {
+        id: 'certificateDelivered',
+        label: 'Certificado entregue',
+        type: 'date',
+        value: formatDatetime(evaluationSheet?.certificateDelivered),
+      },
+      {
+        id: 'dateOfReturn',
+        label: 'Data de retorno',
+        type: 'date',
+        value: formatDatetime(evaluationSheet?.dateOfAssessment),
+      },
+    ]
   }
 
   onSubmit(form: any) {
-    console.log(form.value)
+    if (!this.isUserLevelToEdit) {
+      this.onCancel()
+      return;
+    }
+
+    this.clientModel.evaluationSheet = form.value;
+
+    this.clientModel.evaluationSheet!.practicePhysicalActivity = this.clientModel.evaluationSheet!.physicalActivity !== ''
+
+    console.log(this.clientModel, form.value)
+    const API_CALL = this.clientsService.updateEvaluationSheet(this.clientModel);
+    const existedClientModel = GlobalVariables.clients.find(client => client.clientId === this.id);
+
+    API_CALL.subscribe({
+      next(value) {
+        console.log(value)
+        if (existedClientModel)
+          existedClientModel.evaluationSheet = value.data.evaluationSheet;
+      },
+    })
+  }
+
+  onCancel() {
+    this.routeHistory.navigateBack()
   }
 
 }
